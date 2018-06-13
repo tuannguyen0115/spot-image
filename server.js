006952e5609a49085e1e250de9006c638acf4346
Cani <tuannguyen0115@gmail.com>
2018-06-13 16:21:00 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8000
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.listen(port, () => console.log(`express server listening on port ${port}`))
